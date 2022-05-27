b20edb6c94d208e33eb532429230402d94978489
Rob Carnell <bertcarnell@gmail.com>
2022-05-27 07:33:41 -0400

    In progress commit

---
+++ src/CustomDictionary.R
@@ -0,0 +1,57 @@
+word_list <- c(
+  # places
+  "Nasugbu", "Mangarin", "Caminawit", "Mariveles", "Subic", "Corregidor", "Caballo", "Monja",
+  "Malinta", "Whangpoo", "Fatu", "Tinghai", "Bunji", "Chusan", "Lubang", "Shima", "Nansei",
+  "Shoto", "Havannah", "Efate", "Hilliard", "Bonham", "Fila", "Leyte", "Tolosa",
+  "Punubulu", "Tulagi", "Purvis", "Ulawa", "Tukituki", "Nguna", "Lelepa",
+  "Sealark", "SeaLark", "Shortlands", "Kiang", "CN", "Matson", "Ulithi", "Urushi",
+  "Guiuan", "Roadstead", "Surigao", "Morotai", "Balikpapan", "Tokong", "Sepinggang",
+  "Manggar", "Amedee", "Ile", "Nou", "Dumbea", "Boulari", "Renard", "Savo", "Koli",
+  "Lunga", "Espiritu", "Kokumbona", "Tassafaronga", "Undeka", "Bungana",
+  "Yandina", "Menmui", "Lengo", "Esperance", "Tongatabu", "Gunto", "Tungsha",
+  "Tawi", "Zamboanga", "Basilan", "Sulu", "Lamitan", "Mateo", "Tilic", "Jolo", "Sanga",
+  "Celebes", "Miri", "Lutong", "Halmahera", "Seeadler", "Manus", "Cuyo",
+  "Lingayen", "Abuyog", "Agutaya",
+  # Saufley
+  "SAUFLEY", 
+  # Ship abbreviations
+  # LSM - Landing ship medium
+  # FS - Freight and Supply ship
+  # MTB - Motor Torpedo Boat
+  # LST - Landing Ship, Tank
+  # APD - High Speed Amphibious Transport
+  # YMS - Yard Minesweepers
+  # DMS - Destroyer Minesweeper
+  # LCI - Landing Craft Infantry
+  # LCV - Landing Craft Vehicle
+  # LCT - Landing Craft Tank
+  # LCS - Landing Craft Support
+  # PBY - Patrol Bomber
+  # AMS - Auxiliary Motor Minesweeper
+  # LT - Large Tug (Army)
+  # ARD - Auxilliary Repair Dock
+  "LSM", "FS", "MTB", "LST", "APD", "YMS", "DMS", "LCI", "LCV", "LCT", "LCS",
+  "PBY", "AMS", "TP", "ARD",
+  # Ship terms
+  # SPD - Smokeless Powder stabilized with Diphenolamine
+  "mailgram", "starshell", "starshells", "fuzed", "conn", "Afirm", "afirm",
+  "zigzagging", "zig", "zagging", "minesweeping", "xray", "speedletter",
+  "degaussing", "pre", "mersig",  "portside", "Countermarched", "yds",
+  "george", "rds", "stbd", "despatch", "zag", "superheaters", "prelanding",
+  "drydock", "Vals", "Zekes", "Hypo", "DS", "SPD",
+  # command terms
+  "Comdesron", "ComDesRon", "Desron", "SOPA", "ComTaskFor", "ComTaskForce", "ComTask", "DesDis",
+  "CTF", "CDS", "COMSERONSOPAC", "ComSeronsoPac", "CTG", "Comsopac", "ComSoPac", "CTU", "USNR",
+  # compass
+  "PSC", "PSG", "PGC", "PGS", "pgc", "psc",
+  # People
+  "FECHTELER", "STRUBLE", "Struble", "BERKEY", "BARBEY", "Barbey", "KRISTENSEN",
+  "ILSEMANN", "GRIGGS", "LAMIMAN", "Beaty", "MALPASS", "Homonhon", "Nyquist",
+  "Peyton", "PEYTON", "Tolman", "SCHUTZ", "Ainsworth", "Briscoe", "KILAND",
+  "ROSENDAHL", "NEVILLE", "CONOLLY"
+)
+
+# Radars
+# FD - fire control
+# SG - surface search
+# SC - air search
